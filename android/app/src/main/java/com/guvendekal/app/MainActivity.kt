5ad70923317d0ecb0fe099e333332e3337dc2f53
videoin07-coder <videoin07@gmail.com>
2026-03-17 08:52:11 +0300

    fix: restore expo modules in MainActivity and MainApplication

--- android/app/src/main/java/com/guvendekal/app/MainActivity.kt
+++ android/app/src/main/java/com/guvendekal/app/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.guvendekal.app
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import expo.modules.ReactActivityDelegateWrapper
 
 class MainActivity : ReactActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    setTheme(R.style.AppTheme)
+    super.onCreate(null)
+  }
   override fun getMainComponentName(): String = "main"
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-    DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    ReactActivityDelegateWrapper(
+      this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    )
 }
